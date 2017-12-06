1aeffc80edc37b970d440dade47bf5d77d8a31f7
bhushan <bhushan.s.94@gmail.com>
2017-12-05 23:52:57 -0500

    created smartOff consisting of helper routines

---
+++ smartOff.py
@@ -0,0 +1,79 @@
+# Normalizing features
+from numpy import transpose
+from datetime import datetime
+from keras.models import load_model
+from pandas import DataFrame
+
+def normalizeTable(data, maxUsage = -1):
+    data[0] = data[0] / 12  # Change to 12
+    data[1] = data[1] / 31
+    data[2] = data[2] / 24
+    data[3] = data[3] / 60
+    data[4] = data[4] / 60
+    if maxUsage != -1:
+        data[5] = data[5] / maxUsage
+    return data
+
+def normalizeData(values, maxUsage=-1):
+    transposed = transpose(values)
+    scaledT = normalizeTable(transposed, maxUsage)
+    scaled = transpose(scaledT)
+    return scaled
+
+
+def predictOnNext3Hours(model, s, maxUsage):
+    timeS = datetime.fromtimestamp(float(s))
+    print(timeS)
+    timeList = []
+
+    for i in range(0,180):
+        tList = [7, timeS.day, timeS.hour+ int(i/60), timeS.minute + i % 60, timeS.second]
+        timeList.append(tList)
+    newData1 = DataFrame(timeList)
+    newData = normalizeData(newData1.astype(float).values)
+    pData = newData.reshape((newData.shape[0], 1, newData.shape[1]))
+    prediction = model.predict(pData)
+    usageValues = prediction * maxUsage
+    # print(prediction[:10])
+    #print(usageValues)
+    return usageValues
+
+def predictForSingleTime(model, s, maxUsage):
+    timeS = datetime.fromtimestamp(float(s))
+    print(timeS)
+    timeList = []
+    tList = [7, timeS.day, timeS.hour, timeS.minute, timeS.second]
+    timeList.append(tList)
+    newData1 = DataFrame(timeList)
+    newData = normalizeData(newData1.astype(float).values)
+    pData = newData.reshape((newData.shape[0], 1, newData.shape[1]))
+    prediction = model.predict(pData)
+    usageValues = prediction * maxUsage
+    #print(usageValues[0][0])
+    return isOnOrOff(usageValues[0][0])
+
+# UNCOMMENT APPROPRIATE DEVICE BELOW
+# NOTE THAT THIS SHOULD BE REPLACED BY THRESHOLD
+# BEING COMPUTED FROM TRAINING DATA
+#applianceUsed = 'MICROWAVE'
+applianceUsed = 'TV'
+
+def getThresholdValue():
+    if (applianceUsed == 'TV'):
+        return 1
+    elif (applianceUsed == 'MICROWAVE'):
+        return 2
+    # UNKNOWN DEVICE
+    return 1
+
+def isOnOrOff(usageValue):
+    threshold = getThresholdValue()
+    if (usageValue > threshold):
+        return True
+    return False
+
+def loadMicrowaveModel():
+    return load_model('savedModels/full_Microwave.h5')
+
+def loadTVModel():
+    return load_model('savedModel/full_TV.h5')
